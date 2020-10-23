4fb1f3738d5d716e7ac8527c7d80e5123a0f25fa
Avikam <avikam03@gmail.com>
2020-10-23 18:07:52 +0530

    fixed minor bug

--- src/pages/work.js
+++ src/pages/work.js
@@ -17,7 +17,8 @@ const Resume = () => (
           <title>Work | Avikam Mangla</title>
           <meta name="keywords" content="Avikam, Mangla, Avikam Mangla, Enship, Dpsrkp, DPS, Avikam Mangla DPSRKP, avikammangla  "/>
         </Helmet>
-        <nav className="navbar mx-6 mt-4 mb-6 pb-6" role="navigation" aria-label="main navigation">
+        
+        <nav className="navbar mx-6 mt-4 pb-6" role="navigation" aria-label="main navigation">
             <div class="navbar-brand">
                 <Link class="navbar-item" activeClassName="navbar-item" to="/">
                 <strong>AVIKAM MANGLA</strong>
@@ -44,7 +45,13 @@ const Resume = () => (
             </div>
         </nav>
 
-        <h1 class="title is-1 ml-6 has-text-centered">Coming Soon</h1>
+        <h1 class="title is-1 has-text-centered">Work</h1>
+
+        {/* <div class="columns is-mobile is-centered">
+        <div class="column is-half">
+            <img id="myimage" src="https://avatars0.githubusercontent.com/u/24971199?s=400&u=c81b9b01013e9dcf731a2ce8bea03768f2ee6341&v=4"/>
+        </div>
+        </div> */}
 
 
     </div>
